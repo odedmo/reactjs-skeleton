684add0e3c99514aeb88dfde9c5f9d180cf9ad17
odedmo <odedmoran222@gmail.com>
2018-09-19 22:04:20 +0300

    add loading indication

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,16 +1,32 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import Proptypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
+import LoadingDots from './LoadingDots';
 
-const Header = () => {
-  return (
-    <nav>
-      <NavLink to="/" exact activeClassName="active">Home</NavLink>
-      {" | "}
-      <NavLink  to="/courses" activeClassName="active">Courses</NavLink >
-      {" | "}
-      <NavLink  to="/about" activeClassName="active">About</NavLink >
-    </nav>
-  );
+class Header extends React.Component {
+  render() {
+    return (
+      <nav>
+        <NavLink to="/" exact activeClassName="active">Home</NavLink>
+        {" | "}
+        <NavLink  to="/courses" activeClassName="active">Courses</NavLink >
+        {" | "}
+        <NavLink  to="/about" activeClassName="active">About</NavLink >
+        {this.props.loading && <LoadingDots interval={100} dots={20}/>}
+      </nav>
+    );
+  }
+}
+
+Header.propTypes = {
+  loading: Proptypes.bool.isRequired
 };
 
-export default Header;
+function mapStateToProps(state) {
+  return {
+    loading: state.ajaxCallsInProgress > 0
+  };
+}
+
+export default connect(mapStateToProps)(Header);
